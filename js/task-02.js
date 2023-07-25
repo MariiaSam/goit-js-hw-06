f74151e7f6cc7cfaf61b8ebb4795037b78358d95
MariiaSam <kontragent@ukr.net>
2023-07-25 19:56:07 +0300

    add 2 task

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsItems = ingredients.map((ingredient) => {
+    const item = document.createElement('li');
+      item.textContent = ingredient;
+      item.classList.add('item');
+      return item;
+});
+
+ingredientsList.append(...ingredientsItems);
+
